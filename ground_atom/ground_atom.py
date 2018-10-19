18517fc90dbeb2b912bc864a79bbbb793e9183b4
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-10-19 11:02:54 +0200

    Updated database class to new ground atom data structure.

--- ground_atom/ground_atom.py
+++ ground_atom/ground_atom.py
@@ -7,3 +7,12 @@ class GroundAtom():
     def __str__(self):
         instances_formatted = ','.join(self.instances)
         return '{}({})'.format(self.predicate, instances_formatted)
+
+    def __eq__(self, other):
+        if self.predicate == other.predicate and \
+                self.instances == other.instances and \
+                self.truth_value == other.truth_value:
+
+            return True
+        else:
+            return False
--- test/ground_atom/ground_atom_test.py
+++ test/ground_atom/ground_atom_test.py
@@ -3,13 +3,33 @@ from ground_atom.ground_atom import GroundAtom
 TEST_GROUND_ATOM_1 = 'test(test1)'
 TEST_GROUND_ATOM_2 = 'test(test1,test2)'
 
+one_instance_ground_atom_1 = GroundAtom('test', ['test1'])
+one_instance_ground_atom_2 = GroundAtom('test', ['test2'])
+one_instance_ground_atom_3 = GroundAtom('test', ['test1'], 0.0)
+
+two_instances_ground_atom_1 = GroundAtom('test', ['test1', 'test2'])
+two_instances_ground_atom_2 = GroundAtom('test', ['test1', 'test2'])
+
 
 def test_should_return_str_representation_of_one_instance_ground_atom():
-    one_instance_ground_atom = GroundAtom('test', ['test1'])
-    assert str(one_instance_ground_atom) == TEST_GROUND_ATOM_1
+    assert str(one_instance_ground_atom_1) == TEST_GROUND_ATOM_1
 
 
 def test_should_return_str_representation_of_two_instances_ground_atom():
-    two_instances_ground_atom = GroundAtom('test', ['test1', 'test2'])
-    assert str(two_instances_ground_atom) == TEST_GROUND_ATOM_2
+    assert str(two_instances_ground_atom_1) == TEST_GROUND_ATOM_2
 
+
+def test_should_return_true_if_ground_atoms_represents_the_same_fact():
+    assert two_instances_ground_atom_1 == two_instances_ground_atom_2
+
+
+def test_should_return_false_if_ground_atoms_have_different_truth_value():
+    assert not one_instance_ground_atom_1 == one_instance_ground_atom_3
+
+
+def test_should_return_false_if_ground_atoms_have_different_instance_values():
+    assert not one_instance_ground_atom_1 == one_instance_ground_atom_2
+
+
+def test_should_return_false_if_ground_atoms_have_different_instance_number():
+    assert not one_instance_ground_atom_1 == two_instances_ground_atom_1
