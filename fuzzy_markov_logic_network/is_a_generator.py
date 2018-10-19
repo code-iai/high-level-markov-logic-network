a252b80a5f7003b055796991a937ae8f890d9af6
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-10-19 11:22:58 +0200

    Updated is_a generator

--- fuzzy_markov_logic_network/is_a_generator.py
+++ fuzzy_markov_logic_network/is_a_generator.py
@@ -1,13 +1,19 @@
+from ground_atom.ground_atom import GroundAtom
 from word_net import determine_path_similarity_between_two_concepts
-from ground_atom.ground_atom_builder import get_is_a
+
+__IS_A__ = 'is_a'
 
 
-def get_is_a_ground_atoms(object, concepts):
-    is_a_ground_atoms = {}
+def get_is_a_ground_atoms(a, concepts):
+    is_a_ground_atoms = []
 
     for concept in concepts:
-        similarity = determine_path_similarity_between_two_concepts(object, concept)
-        ground_atom = get_is_a(object, concept)
-        is_a_ground_atoms[ground_atom] = similarity
+        is_a_ground_atoms.append(get_is_a_ground_atom(a, concept))
 
     return is_a_ground_atoms
+
+
+def get_is_a_ground_atom(a, b):
+    similarity = determine_path_similarity_between_two_concepts(a, b)
+
+    return GroundAtom(__IS_A__, [a, b], similarity)
