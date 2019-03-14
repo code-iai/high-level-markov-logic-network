faa82d2adb0635b6e5fc0478c2a03ef8caace5af
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-14 14:38:04 +0100

    feat(markov_logic_network.py): Adds train db and learned mln to project

--- high_level_markov_logic_network/markov_logic_network.py
+++ high_level_markov_logic_network/markov_logic_network.py
@@ -1,3 +1,6 @@
+import io
+import os
+
 from pracmln.mlnquery import MLNQuery
 from pracmln.mlnlearn import MLNLearn
 from pracmln.utils.project import MLNProject
@@ -7,6 +10,7 @@ from pracmln.mln.database import parse_db
 
 class MarkovLogicNetwork:
     def __init__(self, path_to_pracmln_project):
+        self.__path_to_pracmln_project__ = path_to_pracmln_project
         self.pracmln_project = MLNProject.open(path_to_pracmln_project)
         self.__config__ = self.pracmln_project.queryconf
         self.__logic__ = self.__config__.config['logic']
@@ -27,18 +31,41 @@ class MarkovLogicNetwork:
 
         return mln_query.run()
 
-    def learn(self):
+    def learn(self, db_content=None):
         learn_config = self.pracmln_project.learnconf
         logic = learn_config.config['logic']
         grammar = learn_config.config['grammar']
-        mln_name = learn_config.config['mln']
-        db_name = learn_config.config['db']
-        db_content = self.pracmln_project.dbs.get(db_name)
-        mln_text = self.pracmln_project.mlns.get(mln_name)
+        output_filename = learn_config.config['output_filename']
+
+        train_db_name = learn_config.config['db']
+
+        if not db_content:
+            db_content = self.pracmln_project.dbs.get(train_db_name)
+
+        #Load template MLN
+        template_mln_name = learn_config.config['mln']
+        mln_text = self.pracmln_project.mlns.get(template_mln_name)
         parsed_mln = parse_mln(mln_text, logic=logic, grammar=grammar)
+
+        #Load Database
         db = parse_db(parsed_mln, db_content)
+
         mln_learn = MLNLearn(learn_config, mln=parsed_mln, db=db)
 
-        result = mln_learn.run()
+        learned_mln = mln_learn.run()
 
-        return result
+        #Read trained MLN as string
+        string_out = io.BytesIO()
+        learned_mln.write(stream=string_out)
+        learned_mln_text = string_out.getvalue()
+
+        self.pracmln_project.add_db(train_db_name, db_content)
+        self.pracmln_project.add_mln(output_filename, learned_mln_text)
+
+        #Fixes for project saving after the init of MLNLearn, the learnconfig will be messed up which causes
+        #crashes when the project is intended to be saved
+        learn_config.config['mln'] = template_mln_name
+        learn_config.config['db'] = train_db_name
+
+        self.pracmln_project.save(os.path.dirname(self.__path_to_pracmln_project__))
+
