6f9d93c647ef56efd880b34f84e137a630e523ce
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-05-21 11:06:07 +0200

    build(setup.py): Sets version to 1.0.1 and removes prac package as deps

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="high_level_markov_logic_network",
-    version="1.0.0",
+    version="1.0.1",
     author="Sebastian Koralewski",
     author_email="seba@cs.uni-bremen.de",
     description="Provides high level functions to access the main functionality of pracmln.",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/code-iai/high-level-markov-logic-network",
     packages=setuptools.find_packages(exclude=("test",)),
-    install_requires=['pracmln==1.2.3', 'prac==1.0.0'],
+    install_requires=['pracmln==1.2.3'],
     classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
